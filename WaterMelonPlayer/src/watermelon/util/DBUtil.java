294ed98d4b09844409549145c6b72241990cd39d
user <user@DESKTOP-G6RMKGA>
2016-10-24 00:08:51 +0900

    DButil , Filter

---
+++ WaterMelonPlayer/src/watermelon/util/DBUtil.java
@@ -0,0 +1,50 @@
+package watermelon.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBUtil {
+
+	static DataSource ds;
+
+	static {
+		try {
+			Context initContext = new InitialContext();
+			ds = (DataSource) initContext.lookup("java:/comp/env/jdbc/myoracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// ¿¬°á
+	public static Connection getConnection() throws SQLException {
+		return ds.getConnection();
+	}
+
+	// ´Ý±â
+	public static void dbClose(Connection con, Statement st, ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+				rs = null;
+			}
+			if (st != null) {
+				st.close();
+				st = null;
+			}
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
