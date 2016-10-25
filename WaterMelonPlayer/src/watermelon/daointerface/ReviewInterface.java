0f44bb9e2b2a3a60c4971812eb13647e854de97f
Administrator <Administrator@192.168.0.108>
2016-10-25 15:59:34 +0900

    수정

--- WaterMelonPlayer/src/watermelon/daointerface/ReviewInterface.java
+++ WaterMelonPlayer/src/watermelon/daointerface/ReviewInterface.java
@@ -1,24 +1,5 @@
 package watermelon.daointerface;
 
-import java.sql.SQLException;
-import java.util.List;
-
-import watermelon.dto.Review;
-
 public interface ReviewInterface {
-	/**
-	 *  모든 review 검색
-	 */
-	List<Review> selectReview() throws SQLException;
-	
-	/**
-	 * 회원에 따른 review 검색
-	 */
-	List<Review> selectMemberReview(String reviewId) throws SQLException;
-	
-	/**
-	 * 앨범에 따른 review 검색
-	 */
-	List<Review> selectAlbumReview(String reviewAlbum) throws SQLException;
 	
 }	
--- WaterMelonPlayer/src/watermelon/daointerface/SongGenreInterface.java
+++ WaterMelonPlayer/src/watermelon/daointerface/SongGenreInterface.java
@@ -1,13 +1,5 @@
 package watermelon.daointerface;
 
-import java.sql.SQLException;
-
-import watermelon.dto.SongGenre;
-
 public interface SongGenreInterface {
-	/**
-	 * 장르코드에 따른 장르이름
-	 */
-	SongGenre selectGerneName(String genereCode) throws SQLException;
-
+	
 }
