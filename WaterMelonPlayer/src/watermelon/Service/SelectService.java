ba688ee90eef53d865ee189d5e29d9d22b24e1eb
Administrator <Administrator@192.168.0.108>
2016-10-25 12:26:55 +0900

    주석달기

--- WaterMelonPlayer/src/watermelon/Service/SelectService.java
+++ WaterMelonPlayer/src/watermelon/Service/SelectService.java
@@ -13,11 +13,13 @@ public class SelectService {
 	
 	public List<Song> songSelect(String content) {
 		try {
+			//dao 에서 얻어온 list값 list에 담기
 			list = dao.selectSongName(content);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//담은 값 리턴
 		return list;
 	}
 }
