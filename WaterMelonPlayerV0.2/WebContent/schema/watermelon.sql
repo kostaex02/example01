6d77d351b8b93b87f261e6c2b5d61291cbb0dd9e
Administrator <Administrator@192.168.0.108>
2016-10-27 12:01:35 +0900

    dto야 배고파

--- WaterMelonPlayerV0.2/WebContent/schema/watermelon.sql
+++ WaterMelonPlayerV0.2/WebContent/schema/watermelon.sql
@@ -35,12 +35,15 @@ drop table artist;
 
 create sequence album_no_sequence nocache;
 
+
+-- 여기까지
+
 create table album(
-	album_no number primary key
+	album_no number primary key,
 	album_name varchar2(25) not null,
-	song_artist varchar2(25) references artist(artist_no),
+	song_artist number references artist(artist_no),
 	debut_date date,
-	genre_code varchar2(10) references song_genre(genre_code)
+	genre_code varchar2(10) references genre(genre_code)
 );
 
 drop table album;
@@ -48,38 +51,38 @@ drop table album;
 create table album_image(
 	album_imgurl varchar2(50) primary key,
 	album_no number references album(album_no)
-)
+);
 
 drop table album_image;
 
 create sequence song_no_sequence nocache;
 
 create table song(
-	song_no number primary key,
-	song_name varchar2(20) not null, -- 곡 이름
-    artist_code number references artist(artist_no), -- 가수 코드
-    genre_code varchar2(10) references song_genre(genre_code) , -- 장르 코드
-    album_code number references album(album_imgurl), -- 앨범 코드
-    album_imgurl varchar2(50) references album_image(album_imgurl), --이미지 url
-    song_title number default 0); -- 타이틀곡 0이면 일반곡, 1이면 타이틀곡
+   song_no number primary key,
+   song_name varchar2(20) not null, -- 곡 이름
+  artist_code number references artist(artist_no), -- 가수 코드
+  genre_code varchar2(10) references genre(genre_code) , -- 장르 코드
+  album_code number references album(album_no), -- 앨범 코드
+  album_imgurl varchar2(50) references album_image(album_imgurl), --이미지 url
+  song_title number default 0); 
 
-drop table song;
+drop table song_url;
 
 create table song_url(
-	url varchar2(50) primary key, -- song url
-	song_no number references song(song_no)
-)
+   song_url varchar2(50) primary key, -- song url
+   song_no number references song(song_no)
+);
 
 create sequence my_album_no_sequence nocache;
 
 create table my_album(
      my_album_no number primary key,
      user_id varchar2(20) references user_table(user_id), -- 회원 아이디
-     song_name varchar2(20) references song(song_no), -- 곡
-     artist_name varchar2(20) references artist(artist_no), --가수명
-     album_name varchar2(20) references album(album_no), --앨범명
-     url varchar2(20) references song_url(url), -- url
-     imgurl varchar2(20) references song(album_imgurl) -- 이미지 url
+     song_name number references song(song_no), -- 곡
+     artist_name number references artist(artist_no), --가수명
+     album_name number references album(album_no), --앨범명
+     song_url varchar2(20) references song_url(song_url), -- url
+     imgurl varchar2(20) references album_image(album_imgurl) -- 이미지 url
 );
 
 drop table my_album;
@@ -89,9 +92,9 @@ create sequence review_no_sequence nocache;
 create table review(
   review_no number primary key, --리뷰 번호
   review_id varchar2(25) references user_table(user_id),  -- 유저테이블 user_id 참조
-  review_contents varchar2(100) -- 리뷰내용
-  review_date date	 -- 리뷰 100자 이내로 저장
-  );        
+  review_contents varchar2(100), -- 리뷰내용
+  review_date date    -- 리뷰 100자 이내로 저장
+);               
 drop table review;
 
 commit
--- WaterMelonPlayerV0.2/src/watermelon/Service/UserService.java
+++ WaterMelonPlayerV0.2/src/watermelon/Service/UserService.java
@@ -8,11 +8,11 @@ import watermelon.dto.Album;
 import watermelon.dto.User;
 
 public class UserService {
-	static UserDAO dao = new UserDAO();
-	
-	public static String login(String email, String pwd) {
+	UserDAO dao = new UserDAO();
+	User user = new User();
+	public String login(String email, String pwd) {
 	 	try {
-	 		User user  = dao.loginUser(email,pwd);
+	 		user = dao.loginUser(email,pwd);
 			String userName = user.getUserName();
 			return userName;
 		} catch (SQLException e) {
@@ -22,7 +22,7 @@ public class UserService {
 	 	return null;
 	}
 	
-	public static boolean insert(User user){
+	public boolean insert(User user){
 		try {
 			int i = dao.insertUser(user);
 			if(i>0){
@@ -39,27 +39,14 @@ public class UserService {
 	
 	//유저 아이디로 해당아이디의 앨범을 검색해서 리턴
 	public static List<Album> getMyalbum(String id) {
+		
 		//10.26 DAO생성후 추가 요망
 		return null;
 	}
 	
 	//비밀번호 확인후 일치할경우 비밀번호를 새로운 비밀번호로 변경
 	public static boolean pwdChange(String id, String oldPwd, String newPwd) {
-		User user;
-		try {
-			user = dao.loginUser(id, oldPwd);
-			System.out.println("user리턴 : "+user);
-			if (user!=null) {
-				System.out.println("유저서비스");
-				int i = dao.updateUser(newPwd, id);
-				System.out.println("업데이트 리턴 : "+i);
-				if (i>0) {
-					return true;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
+		
+		return true;
 	}
 }
